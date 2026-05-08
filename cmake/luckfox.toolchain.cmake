dbf7ae451b559067e9a7ea37a0e2d75382aed638
nhan <longnhan252@gmail.com>
2026-05-08 18:52:08 +0700

    update cmake

---
+++ cmake/luckfox.toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CROSS_COMPILE arm-rockchip830-linux-uclibcgnueabihf-)
+
+set(CMAKE_C_COMPILER   ${CROSS_COMPILE}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILE}g++)
+set(CMAKE_AR           ${CROSS_COMPILE}ar)
+set(CMAKE_STRIP        ${CROSS_COMPILE}strip)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a -mfpu=neon -mfloat-abi=hard")
